d1b05f6311dc31105c1dd93cffd4a285d8984f47
kajalshanto <hasibulhasan.kajal@gmail.com>
2022-06-15 00:09:05 +0600

    another solution of remove duplicates from sorted array

--- 26_remove-duplicates_from_sorted_array.js
+++ 26_remove-duplicates_from_sorted_array.js
@@ -1,12 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * Time complexity O(n)
+ * Space complexity O(1)
+ */
 var removeDuplicates = function (nums) {
   let i = 0;
+  let j = 1;
 
-  if (nums.length == 0) return 0;
-  for (let j = 1; j < nums.length; j++) {
+  if (nums.length === 0) return 0;
+  for (j; j < nums.length; j++) {
     // check if the next value is not duplicate
     if (nums[i] !== nums[j]) {
-      nums.splice(i, 1);
       i++;
+    }
+    // if duplicate found then change the next value to previou one for next iteration
+    nums[i] = nums[j];
+  }
+  return i + 1;
+};
+
+/**
+ * Another solution
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * Time complexity O(n^2)
+ * Space complexity O(n)
+ */
+var removeDuplicates = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = nums.length; j >= i + 1; j--) {
+      if (nums[i] == nums[j]) {
+        nums.splice(i, 1);
+      }
     }
   }
 };
